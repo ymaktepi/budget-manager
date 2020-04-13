8f1bfdc399f0ed1c2440a6657352dc54c5a7b1ed
Nicolas Huguenin <7804729+ymaktepi@users.noreply.github.com>
2020-04-13 15:24:37 +0200

    Fix/google validation (#14)
    * fix: scope is sensible, not restricted
    
    * fix: more details for google validation

--- src/components/secondary/PrivacyPolicy.tsx
+++ src/components/secondary/PrivacyPolicy.tsx
@@ -8,11 +8,13 @@ class PrivacyPage extends React.Component<any, any> {
         return (
             <MainContainer>
                 <CardWithTitle title={"Privacy Policy"}>
-                    This page explains what kind of data data is collected and what we do with it.
-                    The code is open-source and can be reviewed <Link href={"https://github.com/ymaktepi/budget-manager"}>here</Link>.
+                    This page explains what kind of data data is collected and what we do with it.<br/>
+                    The code is open-source and can be reviewed <Link
+                    href={"https://github.com/ymaktepi/budget-manager"}>here</Link>.
                 </CardWithTitle>
                 <CardWithTitle title={"What data is collected"}>
-                    No personal data is collected. This page is merely a visualisation tool for a Google Spreadsheet. <br/>
+                    No personal data is collected. This page is merely a visualisation tool for a private Google
+                    Spreadsheet. <br/>
                     We only log the following data:
                     <ul>
                         <li>Time and date of access to the website;</li>
@@ -23,6 +25,23 @@ class PrivacyPage extends React.Component<any, any> {
                 <CardWithTitle title={"What we do with this data"}>
                     The logging is there to eventually debug when users encounter errors. We probably
                     won't bother looking at it though.
+                </CardWithTitle>
+                <CardWithTitle title={"Why does this application need to be authorized?"}>
+                    This application needs to be enabled on your Google account because it uses Google Sheets as a
+                    database.
+                    What this application can do is basically create spreadsheets and then edit and delete them. It
+                    cannot
+                    edit or delete spreadsheets that were not created by this application.
+                    What does this application will do with data:
+                    <ul>
+                        <li>Create a spreadsheet to store your expenses;</li>
+                        <li>Add expenses and categories to it;</li>
+                        <li>When "Archiving" expenses, it will duplicate two sheets inside the spreadsheet (current
+                            categories and expenses) and then clear the current expenses.
+                        </li>
+                    </ul>
+                    Your expenses are only stored in a private Google Spreadsheet, and they only transit between
+                    Google's servers and your browser, using HTTPS.
                 </CardWithTitle>
             </MainContainer>
         );
