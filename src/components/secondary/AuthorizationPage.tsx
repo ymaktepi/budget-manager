83718785dad255c9bcfa81a37934996d37cd23bd
Nicolas Huguenin <nicolas@unit8.co>
2020-04-13 15:23:01 +0200

    fix: more details for google validation

--- src/components/secondary/AuthorizationPage.tsx
+++ src/components/secondary/AuthorizationPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {Button} from "@material-ui/core";
+import {Button, Link as MUILink} from "@material-ui/core";
 import {generateToken, getClientFromStorage} from "../../utils/clientUtils";
 import {MainContainer} from "../common/MainContainer";
 import {CardWithTitle} from "../common/Card";
+import {Link} from "react-router-dom";
 
 class AuthorizationPage extends React.Component<any, any> {
     render = () => {
@@ -18,6 +19,7 @@ class AuthorizationPage extends React.Component<any, any> {
                     <Button onClick={() => generateToken()} variant={"outlined"} fullWidth>
                         Authorize this application
                     </Button>
+                    You can take a look at the <Link to={"/privacy"} component={MUILink}>Privacy Policy</Link> for more details.
                 </CardWithTitle>
             </MainContainer>
         );
